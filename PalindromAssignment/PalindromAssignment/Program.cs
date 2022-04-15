91586996997b2b7f81af50744b8b23900b85e340
mmicleus <36306873+mmicleus@users.noreply.github.com>
2022-04-15 11:45:34 +0100

    Palindrom + Bonus

---
+++ PalindromAssignment/PalindromAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    internal class PalindromAssignment
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int length;
+            bool isPalind = true;
+
+            str = Console.ReadLine();
+            length = str.Length;    
+
+            for(int i = 0;i <= length / 2;i++)
+            {
+                if(str[i] != str[length - 1 - i])
+                {
+                    isPalind = false;
+                    break;
+                }
+            }
+
+           string message =  (isPalind == true) ? $"'{str}' este palindrom." : $"'{str}' nu este palindrom";
+
+            Console.WriteLine(message);
+
+        }
+    }
+}
---
+++ PalindromAssignment/PalindromAssignmentBonus/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    internal class PalindromAssignmentBonus
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            StringBuilder sb = new StringBuilder();
+            int length;
+            bool isPalind = true;
+
+            str = Console.ReadLine();
+
+            foreach(char ch in str)
+            {
+                if (Char.IsLetter(ch))
+                    sb.Append(ch);
+            }
+
+
+            length = sb.Length;
+
+            for (int i = 0; i <= length / 2; i++)
+            {
+                if (sb[i] != sb[length - 1 - i])
+                {
+                    isPalind = false;
+                    break;
+                }
+            }
+
+            string message = (isPalind == true) ? $"'{str}' este palindrom." : $"'{str}' nu este palindrom";
+
+            Console.WriteLine(message);
+
+        }
+    }
+}
