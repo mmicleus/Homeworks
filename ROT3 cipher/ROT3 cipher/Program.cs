60398dd36639c1a16fe434aa4c4a77090cdc175f
mmicleus <36306873+mmicleus@users.noreply.github.com>
2022-04-15 10:49:09 +0100

    ROT3 cipher

---
+++ ROT3 cipher/ROT3 cipher/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROT3Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder result = new StringBuilder();
+            string str;
+            char aux;
+
+
+            str = Console.ReadLine();
+
+            foreach(char c in str)
+            {
+                if(c >= 'A' && c <= 'W')
+                {
+                    aux = (char)(c + 3);
+                }
+                else if(c >= 'X' && c <= 'Z')
+                {
+                    aux = (char)('A' + (c - 'X'));
+                }
+                else if (c >= 'a' && c <= 'w')
+                {
+                    aux = (char)(c + 3);
+                }
+                else if (c >= 'x' && c <= 'z')
+                {
+                    aux = (char)('a' + (c - 'x'));
+                }
+                else
+                {
+                    aux = c;
+                }
+
+                result.Append(aux);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
