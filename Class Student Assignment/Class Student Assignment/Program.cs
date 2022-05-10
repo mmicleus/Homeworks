696411ec95e06f3508d455a85bad401f4ae73994
mmicleus <36306873+mmicleus@users.noreply.github.com>
2022-05-10 09:37:11 +0100

    Added Class Student Assignment

---
+++ Class Student Assignment/Class Student Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Class_Student_Assignment;
+
+namespace StudentAssignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student[] studenti;
+            
+            
+            Console.WriteLine("Introduceti numele elevului");
+            string name = Console.ReadLine();
+            Console.WriteLine("Introduceti varsta elevului");
+            byte age = byte.Parse(Console.ReadLine());
+            
+
+            Student st = new Student(age,name);
+
+            Console.WriteLine(st.Info);
+
+        }
+    }
+}
---
+++ Class Student Assignment/Class Student Assignment/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Student_Assignment
+{
+    class Student
+    {
+        private byte _age;
+        private string _name;
+        private const byte MIN_AGE = 18;
+        private const byte MAX_AGE = 99;
+        public double? Mark { get; set; }
+
+
+        public Student(byte age, string name)
+        {
+            Age = age;
+            _name = name;
+        }
+
+        public byte Age
+        {
+            get { return _age; }
+
+            set
+            {
+                if(value >= MIN_AGE && value <= MAX_AGE)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    _age = MIN_AGE;
+                }
+
+            }
+        }
+
+        public dynamic Info
+        {
+            get
+            {
+                return $"{_name} {Age.ToString()}";
+            }
+        }
+    }
+}
